930317891fdf4ab1ca49767abe09d90b073ca455
hisham9393 <67209210+hisham9393@users.noreply.github.com>
2023-08-11 06:57:42 -0400

    profile avatar field updated

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,6 +20,11 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
+                @if (Auth::user()->avatar)
+                <img width="50" height="50" class="rounded-full pb-2" src="{{ "/storage/".Auth::user()->avatar }}" alt="user avatar">
+            @else
+                <img width="70" height="70" class="rounded-full" src="{{ "/storage/avatars/avatar.png" }}" alt="user avatar">
+            @endif
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
